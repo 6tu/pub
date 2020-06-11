f8d1dba155b5deaf3608f56fe354c2fe86b50768
liuyun <15335745+6tu-git@users.noreply.github.com>
2020-06-11 20:34:10 +0800

    Update dellog.sh

--- dellog.sh
+++ dellog.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+du -t 10M /var/log
 
 find /var/log -name '*.gz'    -exec rm -rf {} \;
 find /var/log -name '*.1'     -exec rm -rf {} \;
 find /var/log -name '*-2*'    -exec rm -rf {} \;
+find /var/log -name '*.log'    -exec touch >  {} \;
 find /var/log -type f         -exec ls {} \;
+
 touch > /var/log/messages
 touch > /var/log/btmp
 touch > /var/log/wtmp
